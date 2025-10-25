d8472af6d04083fec72144cf9713d5f45ab5e581
martin-lgtm <martin.stojanov.ben10@gmail.com>
2025-10-25 20:04:53 +0200

    proxy update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
-        //
+public function boot()
+{
+    if (app()->environment('production')) {
+        URL::forceScheme('https');
     }
+}
 }
