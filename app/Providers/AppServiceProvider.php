43ca158f592e1c910782b093a1d079e0ecd897e1
martin-lgtm <martin.stojanov.ben10@gmail.com>
2025-10-26 00:37:18 +0200

    url facade service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,28 +3,25 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // ✅ add this
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-     *
-     * @return void
      */
-    public function register()
+    public function register(): void
     {
         //
     }
 
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
-public function boot()
-{
-    if (app()->environment('production')) {
-        URL::forceScheme('https');
+    public function boot(): void
+    {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
-}
 }
